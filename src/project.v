aca95881644eb5603826ce8b322e9ea57d7f087e
benoitlx <benoitleroux41@gmail.com>
2025-03-23 00:12:18 +0100

    initial test

--- src/project.v
+++ src/project.v
@@ -1,27 +1,38 @@
-/*
- * Copyright (c) 2024 Your Name
- * SPDX-License-Identifier: Apache-2.0
- */
+module tt_um_benoitlx(a,b,op,o);
+    
+    // Module arguments
+    input wire  [7:0] a;
+    input wire  [7:0] b;
+    input wire  [1:0] op;
+    output reg  [7:0] o;
+    
+    // Enums
+    localparam Operation$Addition = 0;
+    localparam Operation$Substraction = 1;
+    localparam Operation$And = 2;
+    localparam Operation$Or = 3;
+    
+    // Update code
+    always @(*) begin
+        case (op)
+            Operation$Addition:
+                begin
+                    o = a + b;
+                end
+            Operation$Substraction:
+                begin
+                    o = a - b;
+                end
+            Operation$And:
+                begin
+                    o = a & b;
+                end
+            Operation$Or:
+                begin
+                    o = a | b;
+                end
+        endcase
+    end
+    
+endmodule // top
 
-`default_nettype none
-
-module tt_um_example (
-    input  wire [7:0] ui_in,    // Dedicated inputs
-    output wire [7:0] uo_out,   // Dedicated outputs
-    input  wire [7:0] uio_in,   // IOs: Input path
-    output wire [7:0] uio_out,  // IOs: Output path
-    output wire [7:0] uio_oe,   // IOs: Enable path (active high: 0=input, 1=output)
-    input  wire       ena,      // always 1 when the design is powered, so you can ignore it
-    input  wire       clk,      // clock
-    input  wire       rst_n     // reset_n - low to reset
-);
-
-  // All output pins must be assigned. If not used, assign to 0.
-  assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
-  assign uio_out = 0;
-  assign uio_oe  = 0;
-
-  // List all unused inputs to prevent warnings
-  wire _unused = &{ena, clk, rst_n, 1'b0};
-
-endmodule
